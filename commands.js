7e4889f9a04a92bebacda16c78115c5f3a7bf6d2
Nikenrhyngty <161337491+Nikenrhyngty@users.noreply.github.com>
2024-03-12 15:51:42 +0700

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,36 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('Pay', () => {
+    
+    cy.clearCookies()
+    cy.clearLocalStorage()
+    cy.get('#sp_amount').type("1000")
+    cy.get('#sp_date').click()
+    cy.get('#sp_description').type("Purchase new phone")
+    cy.get('#pay_saved_payees').click()
+
+})
